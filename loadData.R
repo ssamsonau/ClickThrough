bfbe9244d69c6e470e59fe9af15264f693f6777a
ssamsonau <ssamsonau@gc.cuny.edu>
2014-12-15 18:57:19 -0500

    class probabilities error corrected

--- loadData.R
+++ loadData.R
@@ -1,16 +1,23 @@
 file.path <- "E://Temp/Click-Through-Data/train"
-N.of.clusters <- 3 # Number of clusters for a parralel execution
 
-#library(Revobase);   setMKLthreads(4)
-#parallel processing to be used in caret::train
-library(foreach); library(doParallel)
-if(exists("workers")) stopCluster(workres)
-workers <- makeCluster(N.of.clusters);  registerDoParallel(workers)
+UseParallel = FALSE
+
+if(UseParallel){
+  N.of.clusters <- 3 # Number of clusters for a parralel execution
+  
+  #library(Revobase);   setMKLthreads(4)
+  #parallel processing to be used in caret::train
+  library(foreach); library(doParallel)
+  if(exists("workers")) stopCluster(workers)
+  workers <- makeCluster(N.of.clusters);  registerDoParallel(workers)
+  
+}
+
 
 
 library(data.table)
 
-DT <- fread(file.path, nrows = 10000)
+DT <- fread(file.path, nrows = 200)
 
   # removing variables which expected to introduce noice only
 colToDelete <- c("id", "device_ip", "device_id", "device_model")
@@ -18,10 +25,14 @@ colToDelete <- c("id", "device_ip", "device_id", "device_model")
   # remove hour coloumn for now - when using only small part of data
 colToDelete <- c(colToDelete, "hour")
 
-DT <- DT[, (colToDelete):= NULL]
+  #levels of the outcome will be translated to DF coloumn names while predicting probabilities
+  #- so they should not start from a number
+DT[, click := paste0("L", as.character(click))]
+
+DT[, (colToDelete):= NULL]
 
 DTcol <- names(DT)
-DT <- DT[, (DTcol):= lapply(.SD, as.factor), .SDcols=DTcol]
+DT[, (DTcol):= lapply(.SD, as.factor), .SDcols=DTcol]
 str(DT)
 
 
@@ -30,12 +41,16 @@ library(caret)
 trainCt <- trainControl(method = "boot", 
                         number = 10,
                         classProbs=TRUE,
-                        summaryFunction=twoClassSummary)
+                        summaryFunction=twoClassSummary, 
+                        verboseIter=TRUE,
+                        allowParallel=UseParallel)
 
-mod <- train(click ~ ., data=DT, trControl=trainCt, metric="ROC")
+mod <- train(click ~ ., data=DT, 
+             method="rf",
+             trControl=trainCt, metric="ROC")
 mod
 
 predOut <- predict(mod, newdata=DT)
 print(predOut)
 
-stopCluster(workers)
+if(UseParallel) stopCluster(workers)
