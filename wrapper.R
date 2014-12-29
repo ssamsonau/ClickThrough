b3f3a338eabb888884969a37612462957e9c6f85
ssamsonau <ssamsonau@gc.cuny.edu>
2014-12-29 16:12:53 -0500

    submission 10
    50000, nnet

--- wrapper.R
+++ wrapper.R
@@ -1,8 +1,8 @@
 file.path <- "E://Temp/Click-Through-Data/"
 N_rows <- 50000
-model.name <- "svmLinear"
+model.name <- "nnet"
 
-UseParallel = TRUE
+UseParallel = FALSE
 N.of.clusters <- 3 # Number of clusters for a parralel execution
 if(UseParallel) source("useParallel.R")
 
@@ -17,24 +17,19 @@ if(!exists("vectIDs")){
   setnames(vectIDs, "V1", "id")
 }
 
+#sample to get train data
+vectorChoose <- sample(vectIDs$id, N_rows)
+vectorChooseSTR <- paste(vectorChoose, collapse="','")
 
 #Read subset of the data from db
 library(RSQLite)
 con <- dbConnect("SQLite", dbname= paste0(file.path, "ClickTroughTrain.sqlite") )
 
-#sample to get train data
-vectorChoose <- sample(vectIDs$id, N_rows)
-
-vectorChooseSTR <- ""
-for(val in vectorChoose){
-  vectorChooseSTR <- paste0(vectorChooseSTR, "'", val, "', ")
-}
-vectorChooseSTR <- substr(vectorChooseSTR, 1, nchar(vectorChooseSTR)-2 )
 trainDT <- data.table(
-  dbGetQuery(con, paste0("SELECT * FROM trainTable WHERE id IN (", vectorChooseSTR ,") ;"))
+  dbGetQuery(con, paste0("SELECT * FROM trainTable WHERE id IN ('", vectorChooseSTR ,"') ;"))
 )
-
 dbDisconnect(con)
+
 
 StartTime <- Sys.time()
 source("train_model.R")
