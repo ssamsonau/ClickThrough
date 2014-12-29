fa4b997ac2b31d57d1058a19135861789960246f
ssamsonau <ssamsonau@gc.cuny.edu>
2014-12-28 22:34:18 -0500

    submission 8 corrected

--- combine_predicted.R
+++ combine_predicted.R
@@ -5,10 +5,17 @@ sub456 <- fread("submission_svmLinear_10000_Seed456.csv")
 str(sub123[click==0.5,])
 str(sub456[click==0.5,])
 
-vectNotPredicted <- sub123[, click]==0.5
-  
-sub123[vectNotPredicted, click:=sub456[vectNotPredicted, click] ] 
+vPredict123 <- sub123[, click]!=0.5
+vPredict456 <- sub456[, click]!=0.5  
 
-str(sub123[click==0.5,])
 
-write.table(format(combOut, scientific=FALSE), file="submission_comb.csv", quote=F, row.names=F, sep=",")
+sub123[!vPredict123 & vPredict456, click:=sub456[!vPredict123 & vPredict456, click] ] 
+sub456[!vPredict456 & vPredict123, click:=sub123[!vPredict456 & vPredict123, click] ] 
+
+subComb <- data.table(sub123)
+subComb[, click:=(sub123[, click]+sub456[, click])/2]
+
+
+str(subComb[click==0.5,])
+
+write.table(format(subComb, scientific=FALSE), file="submission_comb.csv", quote=F, row.names=F, sep=",")
