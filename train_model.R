59a5a42eb777c3653b8edf8556ec7c8c5fedae95
ssamsonau <ssamsonau@gc.cuny.edu>
2014-12-29 19:35:41 -0500

    submission 11
    nnet, 5000, size from 7 to 11

--- train_model.R
+++ train_model.R
@@ -7,19 +7,24 @@ source("myCustomClassSummary.R")
 
 #trGrid <-  expand.grid(C=c(0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300))
 #trGrid <-  expand.grid(C=c(0.1, 1, 10))
+trGrid <- expand.grid(size=c(7, 9, 11), decay=c(1e-3, 1e-1, 3e-1, 9e-1))
 
 trainCt <- trainControl(
   method = "cv", number =2,
   summaryFunction = myCustomClassSummary, #multiClassSummary, ## Evaluate performance using the following function
+  #summaryFunction = twoClassSummary,
   classProbs = TRUE,
   verbose=TRUE) 
 
 print(str(trainDT))
 mod <- train(.outcome ~ ., data=trainDT, 
              method=model.name,
-             trControl=trainCt, #tuneGrid = trGrid,
-             metrics="logLoss", maximize=FALSE)
-
+             trControl=trainCt, 
+             tuneGrid = trGrid,
+             #metric = "ROC"
+             metric="logLoss", maximize=FALSE,
+             MaxNWts = 100000
+             )
 print(mod)
 
 save(mod, file = paste0("model_", N_rows, "_", model.name, ".rda"))
