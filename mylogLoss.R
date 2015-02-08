f6e68587e2e131bc198fa542b2631c73e8126f39
ssamsonau <ssamsonau@gc.cuny.edu>
2015-02-08 11:27:46 -0500

    caret nnet cleaning up ..

--- mylogLoss.R
+++
@@ -1,9 +0,0 @@
-mylogLoss <- function(actual, predicted_prob){
-  
-  #print(actual)
-  #print(predicted_prob)
-  score <- -(actual*log(predicted_prob) + (1-actual)*log(1-predicted_prob))
-  score[actual==predicted_prob] <- 0
-  score[is.nan(score)] <- Inf
-  mean(score)
-}
